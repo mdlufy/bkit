0d74525d7bb94c52e991ebbf20fef3a86be4cbed
mdlufy <87331898+mdlufy@users.noreply.github.com>
2021-11-28 15:27:48 +0300

    lab04

---
+++ lab04/main.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Developer(ABC):
+
+    @abstractmethod
+    def factory_method(self):
+        pass
+
+    def some_operation(self) -> str:
+        # Вызываем фабричный метод, чтобы получить объект-продукт.
+        product = self.factory_method()
+
+        # Далее, работаем с этим продуктом.
+        result = f"Creator: Today the company has created a new game: {product.operation()}"
+
+        return result
+
+
+class ComputerDeveloper(Developer):
+    def factory_method(self) -> Game:
+        return ComputerGame()
+
+
+class MobileDeveloper(Developer):
+    def factory_method(self) -> Game:
+        return MobileGame()
+
+
+class Game(ABC):
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+class ComputerGame(Game):
+    def operation(self) -> str:
+        return "{Computer game created}"
+
+
+class MobileGame(Game):
+    def operation(self) -> str:
+        return "{Mobile game created}"
+
+
+def client_code(creator: Developer) -> None:
+
+    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
+          f"{creator.some_operation()}", end="")
+
+
+if __name__ == "__main__":
+    print("App: Launched with the ComputerDeveloper.")
+    client_code(ComputerDeveloper())
+    print("\n")
+
+    print("App: Launched with the MobileDeveloper.")
+    client_code(MobileDeveloper())
+    print("\n")
